917f60511dc0b219c85e71e4c9312ae3f2267556
zeyqam <zegamda@code.edu.az>
2024-02-21 06:50:05 +0400

    hometask

---
+++ Array-Datetime/Program.cs
@@ -0,0 +1,12 @@
+﻿
+//Method yazirsiz, Method stringden ibaret array ve char qebul edir. Method arrayin icerisinde gelen chardan ne qederdirse onlarin sayini geriye qaytarsin.
+int CountCharacter(string[]array,char character)
+{
+    
+    int totalCount=array.Sum(str=>str.Count(c=>c==character));
+    return totalCount;
+}
+string[] array = { "car", "city", "town", "house", "building" };
+char character = 'c';
+int count = CountCharacter(array,character);
+Console.WriteLine($"'{character} charin arrayin icindeki sayi :{count}");
